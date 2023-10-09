4f7c8b2a545848f191b4bf9368017061c86f13e6
santoshskamthe <santoshkamthe9121@gmail.com>
2023-10-09 09:46:12 +0530

    d2b connection nd script

---
+++ Dbconnect.java
@@ -0,0 +1,1 @@
+This file has database connection related script..
---
+++ test.sh
@@ -0,0 +1,10 @@
+echo 'Author:Santosh s Kamthe'
+echo `date`
+echo -e '\n'
+echo "Title:Shell script to print no frm 1-5"
+for (( i=1;i<=5;i++ ))
+do
+	echo $i
+done
+echo -e '\n'
+echo "end of script"
